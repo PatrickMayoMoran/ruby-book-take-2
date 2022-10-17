578cfa677913534f3aa335ec79ffe47712c58bb2
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-10-17 07:15:45 +0000

    change mutate example to not mutate the caller

--- 3_methods/mutate.rb
+++ 3_methods/mutate.rb
@@ -1,9 +1,9 @@
 a = [1, 2, 3]
 
-def mutate(array)
-  array.pop
+def no_mutate(array)
+  array.last
 end
 
-p "Before mutate method: #{a}"
-mutate(a)
-p "After mutate method: #{a}"
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
