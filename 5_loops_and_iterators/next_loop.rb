1512c571e49ad3b72ca41b4a76391873f2b97b1f
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-10-19 17:52:19 +0000

    add more loop/iterator examples

---
+++ 5_loops_and_iterators/next_loop.rb
@@ -0,0 +1,13 @@
+i = 0
+
+loop do
+  i = i + 2
+  if i == 4
+    next
+  elsif i == 10
+   puts i
+   break
+  else
+    puts i
+  end
+end
---
+++ 5_loops_and_iterators/practice_each.rb
@@ -0,0 +1,7 @@
+names = ["Sam", "Mike", "Joe", "Ashley", "Nyki"]
+x = 1
+
+names.each do |name|
+  puts "#{x}. #{name}"
+  x += 1
+end
