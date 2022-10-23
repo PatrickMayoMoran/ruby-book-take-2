08092412fb121ac74483396a30262a4982dc9044
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-10-23 11:13:12 +0000

    add exercise 6 solution

---
+++ 6_arrays/6.rb
@@ -0,0 +1,5 @@
+# The problem is that you are trying to refer to an item
+# in the array with its value directly. That is not how you
+# access an item's value. Instead of using the value, you
+# should use the index position of that item to access
+# and reassign its value.
