951cad22dccd437593a9f116e6866e226581dc6e
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-10-25 16:37:12 +0000

    add exercise 6

---
+++ 7_hashes/6.rb
@@ -0,0 +1,7 @@
+x = "hi there"
+h1 = {x: 12}
+h2 = {x => 12}
+
+# The difference between the two above is that the h1 hash is using the
+# symbol :x as the key whereas h2 is using the string value of x
+# as the key.
