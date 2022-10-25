e541243cae03caa4598e06d59564cab015c4d312
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-10-25 20:17:02 +0000

    add written answer for exercise 3

---
+++ 8_more_stuff/3.rb
@@ -0,0 +1,6 @@
+# Exception handling is dealing with exceptions, or errors, as they are 
+# raised in programs (i.e. as they occur).
+#
+# This solves the problem of programs crashing when exceptions are raised
+# by giving the program a way to handle exceptions and continue executing
+# as opposed to crashing and not finishing.
