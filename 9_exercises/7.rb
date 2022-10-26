a9a1a169c5a2f19f71b64cf077a3511e5e8ae2bb
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-10-26 13:17:59 +0000

    add exercise 7

---
+++ 9_exercises/7.rb
@@ -0,0 +1,3 @@
+# The main difference between a hash and an array is in how values
+# are stored and accessed. Hash values are stored and accessed with keys
+# whereas array values are stored in order and accessed by index number.
