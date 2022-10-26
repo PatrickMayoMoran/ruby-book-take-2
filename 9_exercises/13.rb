863a58039972a95ce006c5d7c908df7846b2972f
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-10-26 16:18:27 +0000

    add exercise 13 part 2 solution

--- 9_exercises/13.rb
+++ 9_exercises/13.rb
@@ -1,4 +1,9 @@
 arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
 
 arr.delete_if { |v| v.start_with?('s') }
-puts arr
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+
+arr.delete_if { |v| v.start_with?('s', 'w') }
+
